946ad5d3049b6f76f905b98c172a50ee72d359ad
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-01-07 10:26:01 +0300

    feat: task reverse-int completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0) {
+        return +(n.toString().split('').splice(1).reverse().join(''));
+    } 
+    return +(n.toString().split('').reverse().join(''));
 }
